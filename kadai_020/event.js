6339a20c7c7f35fe53730f06cd52301965b07459
Sei012Lab <Sei012Lab@gmail.com>
2024-10-13 08:31:15 +0900

    kadai_020のアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const clickBtn = document.getElementById('btn');
+
+clickBtn.addEventListener('click', () => {
+  const changeText = document.getElementById('text');
+  changeText.textContent = 'ボタンをクリックしました';
+});
